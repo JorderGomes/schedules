9ddbc89231ca67eb21c271af75a44f22ea203af3
JorderGomes <jordernoveau@gmail.com>
2025-08-21 17:16:07 -0300

    feat: implementing the isSlotAvailable method

--- src/main/java/com/jorder/schedule/model/AvailabilityEntry.java
+++ src/main/java/com/jorder/schedule/model/AvailabilityEntry.java
@@ -32,8 +32,41 @@ public class AvailabilityEntry {
 	})
 	private TimeRange range;
 	
+	
+	
+	public AvailabilityEntry(UUID id, Weekday weekday, int startTimeHours, int startTimeMinutes, int endTimeHours, int endTimeMinutes) {
+		super();
+		this.id = id;
+		this.weekday = weekday;
+		Time startTime = new Time(startTimeHours, startTimeMinutes);
+		Time endTime = new Time(endTimeHours, endTimeMinutes);
+		this.range = new TimeRange(startTime, endTime);
+	}
+
+
+
 	public boolean isSlotAvailable(CalendarSlot slot) {
+		int slotStartTime = (slot.getStart().getHour() * 60) + slot.getStart().getMinute();
+		int slotEndTime = (slot.getEnd().getHour() * 60) + slot.getEnd().getMinute();
+		
+		int rangeStartTime = range.getStart().getTimeInMinutes();
+		int rangeEndTime = range.getEnd().getTimeInMinutes();
+		
+		if ((slotStartTime >= rangeStartTime) && (slotEndTime <= rangeEndTime)) {
+			return true;
+		}
+		
 		return false;
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
