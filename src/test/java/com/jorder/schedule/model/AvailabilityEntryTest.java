f7f523637dcaa1526efbc46d522df2e032110274
JorderGomes <jordernoveau@gmail.com>
2025-08-22 16:05:32 -0300

    test: implementing tests for the isSlotAvailable method

--- src/test/java/com/jorder/schedule/model/AvailabilityEntryTest.java
+++ src/test/java/com/jorder/schedule/model/AvailabilityEntryTest.java
@@ -1,6 +1,10 @@
 package com.jorder.schedule.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,7 +18,27 @@ class AvailabilityEntryTest {
 	@Test
 	@DisplayName("Should return true when a slot is avaliable")
 	void isSlotAvailableSuccess() {
-		fail("Not yet implemented");
+
+		AvailabilityEntry availabilityEntry = new AvailabilityEntry(UUID.randomUUID(), Weekday.MONDAY, 15, 10, 15, 50);
+
+		CalendarSlot slot = new CalendarSlot(UUID.randomUUID(), LocalDateTime.of(2025, 8, 18, 15, 15), 30);
+
+		boolean isAvailable = availabilityEntry.isSlotAvailable(slot);
+
+		assertTrue(isAvailable);
+	}
+	
+	@Test
+	@DisplayName("Should return false when a slot is avaliable")
+	void isSlotAvailableFail() {
+
+		AvailabilityEntry availabilityEntry = new AvailabilityEntry(UUID.randomUUID(), Weekday.MONDAY, 14, 10, 14, 50);
+
+		CalendarSlot slot = new CalendarSlot(UUID.randomUUID(), LocalDateTime.of(2025, 8, 18, 15, 15), 30);
+
+		boolean isAvailable = availabilityEntry.isSlotAvailable(slot);
+
+		assertFalse(isAvailable);
 	}
 
 }
